caa0cb19b4631ddae17f0c67f1a1a25a4a025a4f
Trevor Parscal <trevorparscal@gmail.com>
2011-10-31 09:44:24 -0700

    Fixed typos

--- isTheDreamOfThe90sAlive.test.js
+++ isTheDreamOfThe90sAlive.test.js
@@ -1,11 +1,11 @@
-module('Distance Test');
+module('Distance Calculation');
 
 test( 'isTheDreamOfThe90sAlive', 5, function() {
 	var tests = {
-		'The dream is alive in NW Porland': [45.57, -122.73, true],
-		'The dream is alive in NE Porland': [45.57, -122.63, true],
-		'The dream is alive in SW Porland': [45.47, -122.73, true],
-		'The dream is alive in SE Porland': [45.47, -122.63, true],
+		'The dream is alive in NW Portland': [45.57, -122.73, true],
+		'The dream is alive in NE Portland': [45.57, -122.63, true],
+		'The dream is alive in SW Portland': [45.47, -122.73, true],
+		'The dream is alive in SE Portland': [45.47, -122.63, true],
 		'The dream is not alive in Beaverton': [45.48, -122.80, false]
 	};
 	for ( var key in tests ) {
